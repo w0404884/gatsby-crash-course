a821d0a9e3ff96fc438264de27e3b52d06440438
w0404884 <ronan.odriscoll@nscc.ca>
2019-06-10 22:33:53 -0300

    Got to 15 minutes in.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Crash Course`,
+    description: `This is a crash course in Gatsby.`,
+    author: `@ronanodriscoll`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-crash-course`,
+        short_name: `crash-course`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula nulla ac gravida lacinia. Nullam pellentesque ac sem eget efficitur. Aliquam venenatis quis orci quis dictum. Integer non tincidunt sem. Nunc vestibulum nibh rutrum diam rhoncus pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+  </Layout>
+)
+
+export default AboutPage
